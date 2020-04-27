21ac757cc6dbbd6ae3006069c0493bc9ca322a6a
Mesi21 <mse212002@gmail.com>
2020-04-27 17:33:00 +0300

    :sparkles: added post model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_27_141657) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
