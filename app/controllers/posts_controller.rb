fb2566df95aaf93418b05d748fc25f2211bf2f57
Mesi21 <mse212002@gmail.com>
2020-04-28 18:51:32 +0300

    :sparkles: added edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all.order('created_at DESC')
+      @posts = Post.all.order('created_at DESC')
     end
 
     def new
@@ -8,18 +8,38 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(post_params)
-        if @post.save
-          redirect_to @post
-        else
-          render 'new'
-        end
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to @post
+      else
+        render 'new'
+      end
     end
 
     def show
-        @post = Post.find(params[:id])
+      @post = Post.find(params[:id])
     end
 
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find(params[:id])
+
+      if @post.update(params[:post]).permit(:title, :body)
+        redirect_to @post
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to root_path
+    end
+    
     private
 
     def post_params
